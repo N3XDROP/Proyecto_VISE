497cfa4ce9c96308fbc83690414d9fc88b246675
José Luis Torres Rivera <torresriverajoseluis9@gmail.com>
2025-10-20 15:41:18 -0500

    Open Telemetry Jose mmds

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,23 @@
 import express from "express";
-import { clientRouter } from "./routes/client";
-import { purchaseRouter } from "./routes/purchase";
+import "./instrumentation";
+
 
 const app = express();
 app.use(express.json());
 
-app.use("/client", clientRouter);
-app.use("/purchase", purchaseRouter);
+
+const PORT: number = parseInt(process.env.PORT || '8082');
+
+function getRandomNumber(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+app.get('/rolldice', (req, res) => {
+  res.send(getRandomNumber(1, 6).toString());
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening for requests on http://localhost:${PORT}`);
+});
 
 export default app;
